c98141f6972e8e18634e3d76b1eeacd7d652b140
mgrybel <monika.grybel@hotmail.com>
2021-01-20 22:36:10 +0000

    Added fonts

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/Open-Sans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
   const [dataLoaded, setDataLoaded] = useState(false);
@@ -8,6 +16,7 @@ export default function App() {
   if (!dataLoaded) {
     return (
       <AppLoading 
+        startAsync={fetchFonts}
         onFinish={() => setDataLoaded(true)} 
         onError={(err) => console.log(err)} 
       />
