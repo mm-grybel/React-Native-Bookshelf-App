499b974436adda8805c983fdab528c46d5f87ca4
mgrybel <monika.grybel@hotmail.com>
2021-01-20 22:23:01 +0000

    Added AppLoading

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading 
+        onFinish={() => setDataLoaded(true)} 
+        onError={(err) => console.log(err)} 
+      />
+    );
+  }
+  
   return (
     <View style={styles.container}>
 
